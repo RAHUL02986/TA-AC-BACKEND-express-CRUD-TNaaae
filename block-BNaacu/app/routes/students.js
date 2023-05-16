88fc44260adfc43c8e3a0e56eda870ea8665598e
Rahul kapoor <kapoorrahul304@gmail.com>
2023-05-16 12:34:58 +0530

    few changes

--- block-BNaacu/app/routes/students.js
+++ block-BNaacu/app/routes/students.js
@@ -1,16 +1,15 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
-
-router.get("/new", (req, res) => {
-  res.render("form");
+router.get('/new', (req, res) => {
+  res.render('studentsForm');
 });
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   res.send(req.body);
 });
 
-router.get("/", (req, res) => {
-  res.render("ls", { list: ["ankit", "suraj", "prashant", "ravi"] });
+router.get('/', (req, res) => {
+  res.render("list", { list: ['ankit', 'suraj', 'prashant', 'ravi'] });
 });
 
 router.get("/:id", (req, res) => {
